22322771445ced52fc7489f3457ed61247d55c65
Nirongkush Chiran <86822535+nfchiran@users.noreply.github.com>
2022-10-09 05:47:02 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+})
+
+//venobox
+$(document).ready(function(){
+    $('.venobox').venobox({
+		spinner		: 'three-bounce',
+		framewidth	:'700px',
+		frameheight	:'400px',
+		border		: '10px',
+		share		: ['facebook', 'twitter', 'linkedin', 'pinterest', 'download'],
+		spinColor	: '#d2d2d2'
+	}); 
+});
